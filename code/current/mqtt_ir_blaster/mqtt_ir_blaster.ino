0ab95e771bcea314587113699af6618ca84013c5
UpbeatOctopus <mason.thomas.kevin@gmail.com>
2020-11-19 21:59:08 -0500

    init, better notes

---
+++ code/current/mqtt_ir_blaster/mqtt_ir_blaster.ino
@@ -0,0 +1,389 @@
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <NTPClient.h>
+#include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <WiFi.h>
+#include "DHT.h"
+
+
+const char* mqttServerIp = "***REMOVED***";
+const short mqttServerPort = 1883;
+const char* mqttUsername = "***REMOVED***";
+const char* mqttPassword = "***REMOVED***";
+const char* mqttClientName = "***REMOVED***";
+
+const char* DATATOPIC = "***REMOVED***/dht22";
+const char* COMMANDTOPIC = "***REMOVED***/command";
+const char* SERVICETOPIC = "***REMOVED***/response";
+const char *willTopic = "***REMOVED***/status";
+const char *willMessage = "offline";
+int willQoS = 1;
+int willRetain = 1;
+bool wasConnected = 0;
+
+#define DHTPIN 15
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+float dht_temp_storage = 0;
+float dht_humid_storage = 0;
+
+
+char *ssid      = "***REMOVED***";               // Set you WiFi SSID
+char *password  = "***REMOVED***";               // Set you WiFi password
+
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+
+IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+WiFiClient espClient;
+PubSubClient mqtt_client(espClient);
+
+//captured by IRrecvDumpV2.ino
+const uint16_t shaw_power_raw[27] = {4950,2002,966,1062,942,3016,914,3042,960,1070,940,1064,938,1092,940,1064,938,1092,940,1064,938,3020,964,1066,940,3016,940};
+const uint16_t shaw_guide[27] = {4976,2000,968,1038,938,3018,992,2968,992,2988,944,3014,968,1038,964,1066,942,1060,966,1066,968,1038,962,2996,992,1036,968};
+const uint16_t shaw_last_channel[27] = {4928,2024,942,1062,940,1090,942,1062,940,3018,964,2994,966,1062,942,1064,966,1062,942,1064,966,2988,968,3016,940,3016,940};
+const uint16_t shaw_go_back[27] = {4956,1998,970,1034,962,2996,994,2964,994,1034,970,2988,936,1094,970,1036,938,1092,970,2988,970,2988,970,2988,970,2988,962};
+
+const uint16_t shaw_enter[27] = {4956,1998,968,1036,992,1038,968,2990,968,2988,916,3042,964,1066,970,1034,962,1068,968,1036,962,2996,994,1036,970,1034,994};
+const uint16_t shaw_arrow_down[27] = {4896,2052,938,1092,942,1064,938,1092,942,1062,916,1116,942,3016,942,1062,960,1072,942,1062,940,3018,966,1062,942,3016,942};
+const uint16_t shaw_arrow_up[27] = {4956,1998,968,2990,970,2988,916,1114,970,1034,968,1036,994,2988,968,1036,958,1072,968,2988,996,2962,968,2988,938,3020,990};
+const uint16_t shaw_arrow_left[27] = {4942,2008,964,2994,994,1036,970,2988,968,2988,938,3020,992,1038,968,1036,992,1038,968,2988,970,1036,994,1036,968,1036,994};
+const uint16_t shaw_arrow_right[27] = {4954,1976,994,2962,994,1036,968,1036,996,1034,970,1036,994,1036,996,1010,1000,1030,968,2988,936,3020,966,1066,968,1036,964};
+
+const uint16_t shaw_1[27] = {4948,2026,942,3016,942,3016,942,3014,940,3018,966,1064,942,1062,964,1066,942,1062,964,1066,942,3014,944,1062,968,1062,942};
+const uint16_t shaw_2[27] = {4956,1998,970,2988,970,1034,994,1038,968,1036,992,2966,994,1034,970,1036,994,1036,968,1036,994,2962,998,2986,970,1034,940};
+const uint16_t shaw_3[27] = {4956,1998,970,1036,960,2998,994,1036,996,1008,992,2966,994,1036,970,1036,994,1034,968,2990,968,2988,964,1068,970,1034,962};
+const uint16_t shaw_4[27] = {4972,1978,994,2964,992,2988,968,2990,968,1036,992,2966,992,1036,970,1036,994,1036,968,1038,994,1036,968,2988,968,2988,962};
+const uint16_t shaw_5[27] = {4978,1998,968,2988,970,1036,994,1036,970,2988,970,2988,938,1092,968,1036,916,1114,996,2962,970,1034,964,2996,994,2964,994};
+const uint16_t shaw_6[27] = {4956,1998,970,2988,968,2990,916,1114,968,2990,968,2988,970,1036,994,1036,968,1036,992,2966,994,2988,968,1036,938,3020,964};
+const uint16_t shaw_7[27] = {4976,1998,968,1036,940,3018,994,1036,970,2988,970,1036,994,1034,968,1036,996,1034,970,2988,970,1034,996,2986,970,2988,968};
+const uint16_t shaw_8[27] = {4974,2000,942,3014,942,1062,966,1064,942,3016,968,1038,992,1038,942,1062,992,1038,968,1038,992,1038,942,1064,992,2966,990};
+const uint16_t shaw_9[27] = {4974,2000,968,1038,990,2968,994,2988,968,2990,968,1036,962,1068,968,1036,962,1068,968,2990,970,1036,992,1036,968,1036,992};
+const uint16_t shaw_0[27] = {4978,1998,968,1036,960,1070,968,2988,970,1034,992,1038,970,2988,970,1036,1022,1008,970,1036,996,2960,996,2988,970,1036,962};
+
+
+WiFiUDP ntpUDP;
+// NTPClient timeClient(ntpUDP);
+NTPClient timeClient(ntpUDP, "192.168.138.1", 3600, 60000);
+
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
+void mqttConnect() {
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (mqtt_client.connect(mqttClientName, mqttUsername, mqttPassword, willTopic, willQoS, willRetain, willMessage)) {
+      Serial.println("Connection Complete");
+      Serial.println(willTopic);
+      mqtt_client.publish(willTopic, "online");
+      // empty out watever command is on the mqtt channel
+      mqtt_client.publish(COMMANDTOPIC, "");
+      // Subcribe here. 
+      mqtt_client.subscribe(COMMANDTOPIC);
+      timeClient.update();
+      int epochDate = timeClient.getEpochTime();
+      DynamicJsonDocument doc(1024);
+      doc["t"] = epochDate;
+      if (wasConnected){
+        doc["n"] = "Lost Connection to mqtt but now im back!";
+      }else{
+        doc["n"] = "First Connection after boot";
+      }
+      char buffer[1024];
+      size_t n = serializeJson(doc, buffer);
+      mqtt_client.publish(SERVICETOPIC, buffer, n);
+      wasConnected = 1;
+      } else {
+      Serial.print("failed, rc = ");
+      Serial.print(mqtt_client.state());
+      Serial.println("Trying again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected!");
+  mqtt_client.setServer(mqttServerIp, mqttServerPort);
+  mqtt_client.setCallback(callback);
+  timeClient.begin();
+  Serial.println("WiFi connected:");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+  irsend.begin();
+  dht.begin();
+	delay(500);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  char *payload_c_string = (char *) payload;
+  char *topic_c_string = (char *) topic;
+  DynamicJsonDocument doc(1024);
+  timeClient.update();
+  int epochDate = timeClient.getEpochTime();
+  doc["t"] = epochDate;
+  if (strcmp((char *) topic_c_string, COMMANDTOPIC) == 0 ) {
+    // Write over doc["n"] later if we actually do something w/ the command.
+    doc["n"] = "Recieved Command but didn't understand";
+
+    char buffer[10];
+    strncpy(buffer, payload_c_string, 7);
+    buffer[7] = '\0';   
+    Serial.println(payload_c_string);
+    if (strcmp((char *) buffer, "channel") == 0 ) {
+      Serial.println("Recived channel change command");
+      char chan_buffer[5];
+      chan_buffer[1] = payload_c_string[8]; 
+      chan_buffer[2] = payload_c_string[9]; 
+      chan_buffer[3] = payload_c_string[10]; 
+      chan_buffer[7] = '\0'; 
+      Serial.println(chan_buffer);
+      for (int i = 1; i < 4; i++){
+        switch(chan_buffer[i]){
+          case '1':
+            irsend.sendRaw(shaw_1, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '2':
+            irsend.sendRaw(shaw_2, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '3':
+            irsend.sendRaw(shaw_3, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '4':
+            irsend.sendRaw(shaw_4, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '5':
+            irsend.sendRaw(shaw_5, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '6':
+            irsend.sendRaw(shaw_6, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '7':
+            irsend.sendRaw(shaw_7, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '8':
+            irsend.sendRaw(shaw_8, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '9':
+            irsend.sendRaw(shaw_9, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          case '0':
+            irsend.sendRaw(shaw_0, 27, 38); 
+            delay(450);
+            Serial.println(chan_buffer[i]);
+            break;
+          default:
+            doc["n"] = "Could not decode number string?";
+            break;
+        }
+      }
+      doc["n"] = chan_buffer;
+    }
+    /// TV Stuff
+    if (strcmp((char *) payload_c_string, "sam_power") == 0 ) {
+      irsend.sendSAMSUNG(0xE0E040BF, 32);
+      doc["n"] = "sam_power";
+    }
+    if (strcmp((char *) payload_c_string, "sam_mute") == 0 ) {
+      irsend.sendSAMSUNG(0xE0E0F00F, 32);
+      doc["n"] = "sam_mute";
+    }
+    if (strcmp((char *) payload_c_string, "sam_source") == 0 ) {
+      irsend.sendSAMSUNG(0xE0E0807F,32,2);
+      doc["n"] = "sam_source";
+    }
+    if (strcmp((char *) payload_c_string, "sam_vol_down") == 0 ) {
+      irsend.sendSAMSUNG(0xE0E0D02F,32,2);
+      doc["n"] = "sam_vol_down";
+    }
+    if (strcmp((char *) payload_c_string, "sam_vol_up") == 0 ) {
+      irsend.sendSAMSUNG(0xE0E0E01F,32,2);
+      doc["n"] = "sam_vol_up";
+    }
+
+
+    if (strcmp((char *) payload_c_string, "ccast_to_sat") == 0 ) {
+      irsend.sendSAMSUNG(0xE0E0807F,32,2);
+      delay(1000);
+      irsend.sendSAMSUNG(0xE0E0807F,32,2);
+      doc["n"] = "Changing from Ccast To Sat";
+    }
+    /////
+    /////
+    /////
+    ///// Shaw Utili
+    /////
+    /////
+    ///// 
+    if (strcmp((char *) payload_c_string, "shaw_power_raw") == 0 ) {
+      irsend.sendRaw(shaw_power_raw, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_power_raw";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_guide") == 0 ) {
+      irsend.sendRaw(shaw_guide, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "Shaw Guide";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_last_channel") == 0 ) {
+      irsend.sendRaw(shaw_last_channel, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_last_channel";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_go_back") == 0 ) {
+      irsend.sendRaw(shaw_go_back, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_go_back";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_enter") == 0 ) {
+      irsend.sendRaw(shaw_enter, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_enter";
+    }
+    /////
+    /////
+    /////
+    ///// Shaw Arrows
+    /////
+    /////
+    /////
+    if (strcmp((char *) payload_c_string, "shaw_arrow_down") == 0 ) {
+      irsend.sendRaw(shaw_arrow_down, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_arrow_down";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_arrow_up") == 0 ) {
+      irsend.sendRaw(shaw_arrow_up, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_arrow_up";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_arrow_left") == 0 ) {
+      irsend.sendRaw(shaw_arrow_left, 27, 38);  // Send a raw data capture  at 38kHz.
+      doc["n"] = "shaw_arrow_left";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_arrow_right") == 0 ) {
+      irsend.sendRaw(shaw_enter, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_arrow_right";
+    }
+    /////
+    /////
+    /////
+    ///// Shaw Numbers
+    /////
+    /////
+    /////
+    if (strcmp((char *) payload_c_string, "shaw_1") == 0 ) {
+      irsend.sendRaw(shaw_1, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_1";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_2") == 0 ) {
+      irsend.sendRaw(shaw_2, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_2";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_3") == 0 ) {
+      irsend.sendRaw(shaw_3, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_3";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_4") == 0 ) {
+      irsend.sendRaw(shaw_4, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_4";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_5") == 0 ) {
+      irsend.sendRaw(shaw_5, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_5";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_6") == 0 ) {
+      irsend.sendRaw(shaw_6, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_6";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_7") == 0 ) {
+      irsend.sendRaw(shaw_7, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_7";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_8") == 0 ) {
+      irsend.sendRaw(shaw_8, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_8";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_9") == 0 ) {
+      irsend.sendRaw(shaw_9, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_9";
+    }
+    if (strcmp((char *) payload_c_string, "shaw_0") == 0 ) {
+      irsend.sendRaw(shaw_0, 27, 38);  // Send a raw data capture at 38kHz.
+      doc["n"] = "shaw_0";
+    }
+
+  }
+  // Prep json
+  char buffer[1024];
+  size_t n = serializeJson(doc, buffer);
+  mqtt_client.publish(SERVICETOPIC, buffer, n);
+  Serial.println();
+  Serial.println();
+}
+
+
+void loop() {
+  delay(1000);
+  if (!mqtt_client.connected()) {
+   mqttConnect();
+  }
+  mqtt_client.loop();
+  DynamicJsonDocument doc(1024);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    // Use the stored value instead
+    // also set error bool
+    char dht_humidity[15];
+    char dht_temp[15];
+    doc["d_t"] = dht_temp_storage;
+    doc["d_h"] = dht_humid_storage;
+    doc["d_e"] = 1;
+
+  } else {
+    char dht_humidity[15];
+    char dht_temp[15];
+    sprintf(dht_temp, "%.1f", t);
+    sprintf(dht_humidity, "%.1f", h);
+    doc["d_t"] = dht_temp;
+    doc["d_h"] = dht_humidity;
+    doc["d_e"] = 0;
+
+    dht_temp_storage = t;
+    dht_humid_storage = h;
+  }
+  timeClient.update();
+  int epochDate = timeClient.getEpochTime();
+  doc["t"] = epochDate;
+  char buffer[1024];
+  size_t n = serializeJson(doc, buffer);
+  mqtt_client.publish(DATATOPIC, buffer, n);
+  mqtt_client.loop();
+
+}
---
+++ index.md
@@ -0,0 +1,78 @@
+## Basic idea outline
+Setup a IR transmitter and linked it up with mqtt. Figured out and saved main functions for TV and satellite, leaned heavily on [this code by crankyoldgit][crankyoldgitIRremoteESP8266] to build out my less cool but more specific version. Specifically take a look at [examples/IRMQTTServer], it's advanced code but fairly useful. Also included a DHT22 to match with the fireplace temps and get a good idea of how the fireplace effects livingroom temps.
+
+### Build
+- **VS1838B Infrared receiver**
+
+- **940nm IR infrared Emission Tube LED**
+   - ForwardVoltage: 0.0-1.3V
+   - ReverseVoltage: 5V
+   - Forward DC: 30mA
+   - Launch Angle: 30
+- NPN transistor
+- ~73ohm resistors
+- DHT22
+
+
+
+
+
+## References
+
+
+- **Information**
+  - github
+    - [SensorsIot/Definitive-Guide-to-IR]
+  - youtube
+    - [Andreas Spiess #171]
+  - e-tinkers
+    - [build-an-esp32-web-server-and-ir-remote]
+- **IR libraries**
+  - [ukw100/IRMP][ukw100IRMP]
+    - Deals w/ samsung well but no shaw, i also don't know if its going to do some raw format or not.
+
+  - [ESP8266-HTTP-IR-Blaster][esp8266HTTPIRBlaster]
+
+  - [crankyoldgit/IRremoteESP8266][crankyoldgitIRremoteESP8266]
+    - This is apperently built specifically for the esp8266, but supports esp32
+    - Looks frequently updated
+    - [IRremoteESP8266/SupportedProtocols.md][irremoteesp8266SupportedProtocols.md]
+    - Was able to capture a raw recording of the sat power button and replay it.
+
+  - [bengtmartensson/Infrared4Arduino][bengtmartenssonInfrared4Arduino]
+      - Recently updated
+      - Looks extensive, has ability to feed to an external analyzer
+
+  - [mdhiggins/ESP8266-HTTP-IR-Blaster][mdhigginsESP8266HTTPIRBlaster]
+
+  - [ExploreEmbedded/Arduino-IRremote][exploreembeddedArduinoIRremote]
+
+
+
+
+
+
+[build-an-esp32-web-server-and-ir-remote]: https://www.e-tinkers.com/2019/11/build-an-esp32-web-server-and-ir-remote/
+
+[irledsAndReceiver]: https://www.amazon.ca/dp/B07FFQ9B9H
+[examples/IRMQTTServer]: https://github.com/crankyoldgit/IRremoteESP8266/tree/master/examples/IRMQTTServer
+
+[Andreas Spiess #171]: https://www.youtube.com/watch?v=gADIb1Xw8PE
+
+[SensorsIot/Definitive-Guide-to-IR]: https://github.com/SensorsIot/Definitive-Guide-to-IR/
+
+[mdhigginsESP8266HTTPIRBlaster]: https://github.com/mdhiggins/ESP8266-HTTP-IR-Blaster/
+[z3t0ArduinoIRremote]: https://github.com/z3t0/Arduino-IRremote
+[sensorsiotESP32IRremote]: https://github.com/SensorsIot/Definitive-Guide-to-IR/tree/master/ESP32-IRremote
+[sensorsiotIRMQTT_ESP32]: https://github.com/SensorsIot/Definitive-Guide-to-IR/tree/master/IRMQTT_ESP32
+[crankyoldgitIRremoteESP8266]: https://github.com/crankyoldgit/IRremoteESP8266
+
+[bengtmartenssonInfrared4Arduino]: https://github.com/bengtmartensson/Infrared4Arduino
+
+[irremoteesp8266SupportedProtocols.md]: https://github.com/crankyoldgit/IRremoteESP8266/blob/master/SupportedProtocols.md
+
+[exploreembeddedArduinoIRremote]: https://github.com/ExploreEmbedded/Arduino-IRremote
+
+[esp8266HTTPIRBlaster]: https://github.com/mdhiggins/ESP8266-HTTP-IR-Blaster/
+
+[ukw100IRMP]: https://github.com/ukw100/IRMP
